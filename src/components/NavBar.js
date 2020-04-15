9af84970e1bfa9f40660edd2e0fecddff5cb6780
Njoku Emmanuel <kalunjoku123@gmail.com>
2020-04-15 19:49:55 +0100

    conditionally render svg

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -22,7 +22,14 @@ export default function NavBar() {
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="0 0 20 20"
               >
-                <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+                {isOpen ? (
+                  <path
+                    xmlns="http://www.w3.org/2000/svg"
+                    d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z"
+                  />
+                ) : (
+                  <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+                )}
               </svg>
             </button>
           </div>
